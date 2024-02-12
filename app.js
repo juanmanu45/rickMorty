64a2c3b209cd2af3653fa2ecc79a980965d9fc2d
Juan Diaz <juanmanu45@gmail.com>
2024-02-12 12:50:55 -0500

    Cambiar a node para iniciar en producción

--- app.js
+++ app.js
@@ -14,170 +14,100 @@ function primos(num) {
     return num > 1;
 }
 
+async function crearContactosYCompanias(res) {
 
-async function crearContactsConPersonajes(res) {
-    try {
-        const response = await axios.get('https://rickandmortyapi.com/api/character');
-        const personajes = response.data.results.filter(personaje => (primos(personaje.id)) || personaje.id === 1);
-        const contacts = [];
-
-
-        for (const personaje of personajes) {
-            const properties = {
-                character_id: personaje.id,
-                firstname: personaje.name,
-                lastname: personaje.name,
-                status_character: personaje.status,
-                character_species: personaje.species,
-                character_gender: personaje.gender
-
-            };
-
-            const contact = await hubspotClient.crm.contacts.basicApi.create({ properties });
-
-            contacts.push({
-                ContactID: contact.id,
-                characterid: properties.character_id,
-                firstName: properties.firstname,
-                lastName: properties.lastname,
-                statusCharacter: properties.status_character,
-                characterSpecies: properties.character_species,
-                characterGender: properties.character_gender
-            });
-        }
-        res.json(contacts);
-    } catch (error) {
-
-        res.status(500).send('Error al crear Contacts en HubSpot', error)
-    }
-}
-
-async function crearCompaniesConLocations(res) {
-    try {
-        const response = await axios.get('https://rickandmortyapi.com/api/location');
-        const locations = response.data.results;
-        const companies = [];
-
-        for (const location of locations) {
-            const properties = {
-                location_id: location.id,
-                name: location.name,
-                location_type: location.type,
-                dimension: location.dimension,
-                creation_date: location.created
-
-            };
-
-            const company = await hubspotClient.crm.companies.basicApi.create({ properties });
-
-            companies.push({
-                CompanyID: company.id,
-                location_id: properties.location_id,
-                name: properties.name,
-                location_type: properties.location_type,
-                dimension: properties.dimension,
-                creation_date: properties.creation_date
-            });
-
-        }
-
-        res.json(companies);
-
-    } catch (error) {
-        console.error('Error al crear Companies en HubSpot', error);
-        res.status(500).send('Error al crear Companies en HubSpot');
-    }
-}
-
-async function asociarContactosConCompanias(res) {
     try {
 
         const { data: locationsData } = await axios.get('https://rickandmortyapi.com/api/location');
         const { data: charactersData } = await axios.get('https://rickandmortyapi.com/api/character');
-        
+
         const characters = charactersData.results.filter(character => primos(character.id) || character.id === 1);
         const locations = locationsData.results;
 
         const companies = [];
         const contacts = [];
+        var contact;
+        var company;
 
-        for (const location of locations) {
-            const properties = {
-                location_id: location.id,
-                name: location.name,
-                location_type: location.type,
-                dimension: location.dimension,
-                creation_date: location.created
+        try {
+            for (const location of locations) {
+                const properties = {
+                    location_id: location.id,
+                    name: location.name,
+                    location_type: location.type,
+                    dimension: location.dimension,
+                    creation_date: location.created
 
-            };
+                };
 
-            const company = await hubspotClient.crm.companies.basicApi.create({ properties });
+                company = await hubspotClient.crm.companies.basicApi.create({ properties });
 
-            companies.push({
-                CompanyID: company.id,
-                location_id: properties.location_id,
-                name: properties.name,
-                location_type: properties.location_type,
-                dimension: properties.dimension,
-                creation_date: properties.creation_date
-            });
+                companies.push({
+                    CompanyID: company.id,
+                    location_id: properties.location_id,
+                    name: properties.name,
+                    location_type: properties.location_type,
+                    dimension: properties.dimension,
+                    creation_date: properties.creation_date
+                });
 
+            }
+        } catch (error) {
+            res.status(500).json('error al crear Companies:', error)
         }
 
-        for (const character of characters) {
-            const properties = {
-                character_id: character.id,
-                firstname: character.name,
-                lastname: character.name,
-                status_character: character.status,
-                character_species: character.species,
-                character_gender: character.gender
+        try {
 
-            };
+            for (const character of characters) {
+                const fullName = character.name.split(" ");
+                const firstName = fullName[0];
+                const lastName =fullName[1]; 
+                
+                const properties = {
+                    character_id: character.id,
+                    firstname: firstName,
+                    lastname: lastName,
+                    status_character: character.status,
+                    character_species: character.species,
+                    character_gender: character.gender
 
-            const contact = await hubspotClient.crm.contacts.basicApi.create({ properties });
+                };
 
-            contacts.push({
-                ContactID: contact.id,
-                characterid: properties.character_id,
-                firstName: properties.firstname,
-                lastName: properties.lastname,
-                statusCharacter: properties.status_character,
-                characterSpecies: properties.character_species,
-                characterGender: properties.character_gender
-            });
+                contact = await hubspotClient.crm.contacts.basicApi.create({ properties });
+
+                contacts.push({
+                    ContactID: contact.id,
+                    characterid: properties.character_id,
+                    firstName: properties.firstname,
+                    lastName: properties.lastname,
+                    statusCharacter: properties.status_character,
+                    characterSpecies: properties.character_species,
+                    characterGender: properties.character_gender
+                });
+            }
+
+        } catch (error) {
+            res.status(500).json('Error al crear Contacts:', error)
         }
-
-
-        
-        await hubspotClient.crm.associations.v4.basicApi.create(
-            'companies',
-            createCompanyResponse.id,
-            'contacts',
-            createContactResponse.id,
-            [
-                {
-                      "associationCategory": "HUBSPOT_DEFINED",
-                      "associationTypeId": AssociationTypes.companyToContact 
-                      // AssociationTypes contains the most popular HubSpot defined association types
-                }
-            ]
-        )
+     
+        res.status(200).json({ companies, contacts })  
     } catch (error) {
-        console.error('Error al asociar contactos con compañías:', error);
+        console.error('Error al invocar el API:', error);
     }
 }
 
 
-app.get('/companies', async (req, res) => {
-    await crearCompaniesConLocations(res);
+app.post('/webhook', (req, res) => {
+    console.log('Datos recibidos:', req.body);
+    // Aquí puedes procesar los datos recibidos en req.body
+    res.status(200).send({ message: 'Datos recibidos con éxito' });
+  });
+
+
+app.get('/create', async (req, res) => {
+    await crearContactosYCompanias(res);
 });
 
-app.get({});
-
-app.get('/contacts', async (req, res) => {
-    await crearContactsConPersonajes(res);
-});
 
 app.get('/characters', async (req, res) => {
     try {
